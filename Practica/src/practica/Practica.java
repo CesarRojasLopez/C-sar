a09b2d696acaa663f2e281f797642fa0af99524d
CV <CV@192.168.1.7>
2017-09-23 13:45:15 -0600

    Se Agrego Nuevas modificacion

---
+++ Practica/src/practica/Practica.java
@@ -0,0 +1,22 @@
+
+package practica;
+
+
+public class Practica {
+
+    
+    public static void main(String[] args) {
+        
+      
+        
+        for(int i = 0 ; i <5; i++){
+            System.out.println("El valor es: "+i+1);
+        }
+        
+        int i = 0;
+        
+        
+       
+    }
+    
+}
